69022a3f1eb8520f39587bc77a58d6d005f12df6
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-15 00:29:00 -0300

    feat: :sparkles: Add new feats to autofix code
    Organize imports
    Prettier
    Eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ['./src/**/*.{html,ts}', './index/html'],
+  darkMode: 'class',
+  content: ['./src/**/*.{html,ts,scss,css}', './index/html'],
   theme: {
-    fontFamily: { sans: ["Inter", "sans-serif"] },
+    fontFamily: { sans: ['Inter', 'sans-serif'] },
     extend: {
       backgroundImage: {
-        "asce": "url('../public/bg-asce.png')",
-        "bg-gradient":
-          "linear-gradient(89.86deg, #9572fc 20.08%, #45e7ad 39.94%, #e1055e 75.57%)",
-        "bg-gradient-100":
-          "linear-gradient(188deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.88%)",
-
+        asce: "url('../public/bg-asce.png')",
+        'bg-gradient':
+          'linear-gradient(89.86deg, #9572fc 20.08%, #45e7ad 39.94%, #e1055e 75.57%)',
+        'bg-gradient-100':
+          'linear-gradient(188deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.88%)',
       },
     },
   },
   plugins: [],
-}
+};
