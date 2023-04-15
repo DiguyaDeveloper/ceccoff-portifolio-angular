49f80e827012f95a07c45070e921125ef4cc5903
Rafael Henrique da Silva <31450046+rafaelhalder@users.noreply.github.com>
2023-04-14 23:57:48 -0300

    feat: :sparkles: updates

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
   selector: 'ceccoff-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  imports: [MatIconModule],
 })
 export class FooterComponent {}
